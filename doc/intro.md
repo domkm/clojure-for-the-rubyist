826bd1c105e675267872267716f1f1d4399ee59e
Dom Kiva-Meyer <git@domkm.com>
2013-11-14 22:15:18 -0800

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-for-the-rubyist
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_for_the_rubyist/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-for-the-rubyist.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-for-the-rubyist.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
