dcf4ec24240b830101b6d428cf1322b114546a8b
Dom Kiva-Meyer <git@domkm.com>
2013-11-18 16:20:43 -0800

    add core.async, url, and description

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clojure-for-the-rubyist "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Resources for teaching Clojure to Rubyists...duh"
+  :url "http://github.com/DomKM/clojure-for-the-rubyist"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]])
